4c362ddc1d3c59526701d7c58c8eddc9c80a3775
cotontigeh <cotontigeh@gmail.com>
2024-06-17 13:32:53 +0200

    update config: nvim

--- nvim/.config/nvim/lua/core/plugins_config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins_config/telescope.lua
@@ -1,3 +1,16 @@
 local builtin = require('telescope.builtin')
+
+require('telescope').setup{
+      defaults = {
+      },
+      pickers = {
+        find_files = {
+          hidden = true
+        }
+      },
+      extensions = {
+      }
+    }
+
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 
