dd582a72094abe009c2491d934d9c5659de23a31
cotontigeh <cotontigeh@gmail.com>
2024-06-19 13:24:00 +0200

    update config: nvim symbol-usage

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -21,6 +21,16 @@ vim.opt.expandtab = true
 vim.cmd [[ set noswapfile ]]
 vim.cmd [[ set termguicolors ]]
 
+-- Saving the old way
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a")
+vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>^V")
+
+-- Yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
 --Line numbers
 vim.wo.number = true
 
